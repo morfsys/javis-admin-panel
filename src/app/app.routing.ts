61d898efc79621188f30264133962f11b0aad243
NEMEXIS\Nidhil <morfsys@gmail.com>
2018-12-14 13:04:55 +0530

    Initial commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Layouts
+import { CommonLayoutComponent } from './common/common-layout.component';
+import { AuthenticationLayoutComponent } from './common/authentication-layout.component';
+
+export const AppRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+    },
+    {
+        path: '',
+        component: CommonLayoutComponent,
+        children: [
+            {
+                path: 'dashboard',
+                loadChildren: './dashboard/dashboard.module#DashboardModule'
+            }
+        ]
+    }
+];
+
